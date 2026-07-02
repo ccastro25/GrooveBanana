cdcb4028472b11c89fab8b4f1ebf624d7543a5ea
Caonabo Castro <caonabocastro25@gmail.com>
2026-07-02 18:16:25 -0400

    adding sketch with piano, bass and drums

--- grooveBan/grooveBan_with_piano_bass_drums/grooveBan_with_piano_bass_drums.ino
+++ grooveBan/grooveBan_with_piano_bass_drums/grooveBan_with_piano_bass_drums.ino
@@ -5,21 +5,21 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-// GUItool: begin automatically generated code
-AudioPlaySdWav           KickSDWay;     //xy=179,434
-AudioPlaySdWav           BassSdWav4;     //xy=182,481
-AudioPlaySdWav           HigHatSdWav3;     //xy=185,381
-AudioPlaySdWav           SnareSdWav2;     //xy=190,331
-AudioPlaySdWav           pianoSdWav6;     //xy=192,614
-AudioPlaySdWav           PianoSdWav9;     //xy=199,747
-AudioPlaySdWav           PianoSdWav10;    //xy=203,814
-AudioPlaySdWav           PianoSdWav7;     //xy=204,657
-AudioMixer4              Pianomixer3;         //xy=385,746
-AudioMixer4              Bassmixer4;         //xy=407,450
-AudioMixer4              Pianomixer1;         //xy=410,642
-AudioMixer4              mixer2;         //xy=431,329
-AudioMixer4              mixer5;         //xy=632,538
-AudioOutputI2S           i2s1;           //xy=837,482
+// --- Audio Objects ---
+AudioPlaySdWav           KickSDWay;
+AudioPlaySdWav           BassSdWav4;
+AudioPlaySdWav           HigHatSdWav3;
+AudioPlaySdWav           SnareSdWav2;
+AudioPlaySdWav           pianoSdWav6;
+AudioPlaySdWav           PianoSdWav9;
+AudioPlaySdWav           PianoSdWav10;
+AudioPlaySdWav           PianoSdWav7;
+AudioMixer4              Pianomixer3;
+AudioMixer4              Bassmixer4;
+AudioMixer4              Pianomixer1;
+AudioMixer4              mixer2;
+AudioMixer4              mixer5;
+AudioOutputI2S           i2s1;
 AudioConnection          patchCord1(KickSDWay, 0, Bassmixer4, 0);
 AudioConnection          patchCord2(KickSDWay, 1, Bassmixer4, 1);
 AudioConnection          patchCord3(BassSdWav4, 0, Bassmixer4, 2);
@@ -42,74 +42,60 @@ AudioConnection          patchCord19(Pianomixer1, 0, mixer5, 2);
 AudioConnection          patchCord20(mixer2, 0, mixer5, 0);
 AudioConnection          patchCord21(mixer5, 0, i2s1, 0);
 AudioConnection          patchCord22(mixer5, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=432,159
-// GUItool: end automatically generated code
+AudioControlSGTL5000     sgtl5000_1;
 
-// 1. Setup Encoders
+// --- Hardware Inputs ---
 Encoder enc1(26, 27);
 Encoder enc2(28, 29);
-
-// 2. Setup Buttons
 const int buttonPins[] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 40, 41};
 const int numButtons = 11;
-
-// Timing variables for non-blocking debounce
-unsigned long lastDebounceTime[11] = {0}; 
-const unsigned long debounceDelay = 50; // 50ms is usually enough for buttons
-
-long pos1 = -999, pos2 = -999;
+unsigned long lastDebounceTime[11] = {0};
+const unsigned long debounceDelay = 50;
 
 void setup() {
   Serial.begin(9600);
+  AudioMemory(16);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.5);
   
-  // Set all buttons as input with internal pull-up
+  if (!(SD.begin(BUILTIN_SDCARD))) {
+    Serial.println("SD Card Error: Check connection/format");
+  }
+
   for (int i = 0; i < numButtons; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
 }
 
 void loop() {
-  // Always check inputs
   handleEncoders();
   handleButtons();
 }
 
 void handleEncoders() {
-  long newPos1 = enc1.read();
-  if (newPos1 != pos1) {
-    pos1 = newPos1;
-    // USE THIS: Map encoder 1 to change a parameter (e.g., volume or pitch)
-  }
-
-  long newPos2 = enc2.read();
-  if (newPos2 != pos2) {
-    pos2 = newPos2;
-    // USE THIS: Map encoder 2 to change a different parameter (e.g., effect intensity)
-  }
+  // Add parameter logic (volume/pitch) here using enc1.read() and enc2.read()
 }
 
 void handleButtons() {
   unsigned long currentMillis = millis();
-
   for (int i = 0; i < numButtons; i++) {
-    if (digitalRead(buttonPins[i]) == LOW) { // Button pressed
-      if (currentMillis - lastDebounceTime[i] > debounceDelay) {
-        
-        // --- TRIGGER LOGIC ---
-        // You can use a switch(i) statement here to map specific pins to specific sounds
-        triggerInstrument(i);
-        
-        lastDebounceTime[i] = currentMillis;
-      }
+    if (digitalRead(buttonPins[i]) == LOW && (currentMillis - lastDebounceTime[i] > debounceDelay)) {
+      triggerInstrument(i);
+      lastDebounceTime[i] = currentMillis;
     }
   }
 }
 
 void triggerInstrument(int index) {
-  // Example: Pins 30-35 are Keyboard, 36-41 are Drums
-  if (index < 6) {
-    // Play Note logic
-  } else {
-    // Play Drum Sample logic
+  Serial.print("Triggering Button: "); Serial.println(index); // Debugging
+  switch(index) {
+    case 0: pianoSdWav6.play("P1.WAV"); break;
+    case 1: PianoSdWav9.play("P2.WAV"); break;
+    case 2: PianoSdWav10.play("P3.WAV"); break;
+    case 3: PianoSdWav7.play("P4.WAV"); break;
+    case 6: KickSDWay.play("KICK.WAV"); break;
+    case 7: SnareSdWav2.play("SNARE.WAV"); break;
+    case 8: HigHatSdWav3.play("HAT.WAV"); break;
+    case 9: BassSdWav4.play("BASS.WAV"); break;
   }
 }
