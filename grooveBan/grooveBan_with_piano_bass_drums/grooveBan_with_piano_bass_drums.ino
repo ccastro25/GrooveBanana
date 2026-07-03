734fe856ae353d55d137386e6a402ab8d12ec1e9
Caonabo Castro <caonabocastro25@gmail.com>
2026-07-03 02:37:02 -0400

    added keychange and volume fuctions

--- grooveBan/grooveBan_with_piano_bass_drums/grooveBan_with_piano_bass_drums.ino
+++ grooveBan/grooveBan_with_piano_bass_drums/grooveBan_with_piano_bass_drums.ino
@@ -4,7 +4,7 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
-
+//SD.open("looper/loop1.wav", FILE_WRITE)
 // --- Audio Objects ---
 AudioPlaySdWav           KickSDWay;
 AudioPlaySdWav           BassSdWav4;
@@ -20,6 +20,8 @@ AudioMixer4              Pianomixer1;
 AudioMixer4              mixer2;
 AudioMixer4              mixer5;
 AudioOutputI2S           i2s1;
+
+// --- Patch Cords ---
 AudioConnection          patchCord1(KickSDWay, 0, Bassmixer4, 0);
 AudioConnection          patchCord2(KickSDWay, 1, Bassmixer4, 1);
 AudioConnection          patchCord3(BassSdWav4, 0, Bassmixer4, 2);
@@ -52,6 +54,24 @@ const int numButtons = 11;
 unsigned long lastDebounceTime[11] = {0};
 const unsigned long debounceDelay = 50;
 
+// --- Variables ---
+long pos1 = -999;
+int currentKeyIndex = 0;
+long lastEncPos = 0;
+const char* keyNames[] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+/*
+  1 skip an inex 
+  2 skip an index
+  3 go to the next index
+  4 skip an index
+  5 skip an index
+  6 skip an inex 
+  7 skip an index
+  8 next index but octave
+
+
+  
+*/
 void setup() {
   Serial.begin(9600);
   AudioMemory(16);
@@ -59,7 +79,8 @@ void setup() {
   sgtl5000_1.volume(0.5);
   
   if (!(SD.begin(BUILTIN_SDCARD))) {
-    Serial.println("SD Card Error: Check connection/format");
+    Serial.println("SD Card Error: Check if FAT32 formatted");
+    while(1); 
   }
 
   for (int i = 0; i < numButtons; i++) {
@@ -68,12 +89,9 @@ void setup() {
 }
 
 void loop() {
-  handleEncoders();
+  handleVolume();
+  ChangeKey();
   handleButtons();
-}
-
-void handleEncoders() {
-  // Add parameter logic (volume/pitch) here using enc1.read() and enc2.read()
 }
 
 void handleButtons() {
@@ -87,15 +105,34 @@ void handleButtons() {
 }
 
 void triggerInstrument(int index) {
-  Serial.print("Triggering Button: "); Serial.println(index); // Debugging
   switch(index) {
-    case 0: pianoSdWav6.play("P1.WAV"); break;
-    case 1: PianoSdWav9.play("P2.WAV"); break;
-    case 2: PianoSdWav10.play("P3.WAV"); break;
-    case 3: PianoSdWav7.play("P4.WAV"); break;
-    case 6: KickSDWay.play("KICK.WAV"); break;
-    case 7: SnareSdWav2.play("SNARE.WAV"); break;
-    case 8: HigHatSdWav3.play("HAT.WAV"); break;
-    case 9: BassSdWav4.play("BASS.WAV"); break;
+    case 0: pianoSdWav6.play("Instruments/piano/P_E.WAV"); break;// pin 30
+    case 1: PianoSdWav9.play("Instruments/piano/P_C.WAV"); break;//// pin 31
+    case 2: PianoSdWav10.play("Instruments/piano/P_G.WAV"); break;//// pin 32
+    case 3: PianoSdWav7.play("Instruments/piano/P_B.WAV"); break;// pin 30
+    case 6: KickSDWay.play("Instruments/drums/kick.WAV"); break;
+    case 7: SnareSdWav2.play("Instruments/drums/snare.WAV"); break;
+    case 8: HigHatSdWav3.play("Instruments/drums/hi-hat.WAV"); break;
+  }
+}
+
+void handleVolume() {
+  long newPos1 = enc1.read();
+  if (newPos1 != pos1) {
+    pos1 = newPos1;
+    float newVolume = constrain(pos1 / 100.0, 0.0, 1.0);
+    sgtl5000_1.volume(newVolume);
+  }
+}
+
+void ChangeKey() {
+  long newPos = enc2.read();
+  int delta = (newPos - lastEncPos) / 4; 
+  if (delta != 0) {
+    currentKeyIndex = (currentKeyIndex + delta) % 12;
+    if (currentKeyIndex < 0) currentKeyIndex += 12;
+    lastEncPos = newPos;
+    Serial.print("Key: "); 
+    Serial.println(keyNames[currentKeyIndex]);
   }
 }
